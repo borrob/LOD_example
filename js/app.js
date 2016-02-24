d0e01a492c3e2847042a99ff9bd849d783cf2f9f
Rob van Loon <borrob@me.com>
2016-02-24 19:04:40 +0100

    BAG zoekactie via JS
    Opschoonwerk van de javascript main file.
    BAG zoekactie neemt nu postcode en huisnummer als input en voegt daarna stuk
    voor stuk de gevonden geometrieeen toe aan de kaart.

--- js/app.js
+++ js/app.js
@@ -5,8 +5,6 @@ var rdProjection;
 var brtAchtgrondPastelOptions;
 var brtAchtergrondPastel;
 
-var wkt;
-var feature;
 var pandenKaartlaag;
 var pandenKaartlaagSource;
 
@@ -19,6 +17,7 @@ function initApp() {
 	initVars();
 	initMap();
 	defineLayers();
+
 	$('#getBAG').click(function(){
 		getBAGpand();
 	});
@@ -51,15 +50,6 @@ function defineLayers() {
 	//define all the layers and load them on the map
 
 	//pandenkaartlaag
-	/*
-	emptywkt=null;
-	var format = new ol.format.WKT();
-	feature = format.readFeature(emptywkt, {
-		dataProjection: 'EPSG:4326',
-		featureProjection: 'EPSG:28992'
-	});
-	*/
-
 	pandenKaartlaagSource = new ol.source.Vector({
 		//features: [feature]
 	});
@@ -106,28 +96,34 @@ function removeAllFromPandenKaartlaag() {
 	}
 }
 
-function addWKTtoPandenKaartlaag(){
+function addWKTtoPandenKaartlaag(addWKT){
 	//read WKT and add it to the map (and zoom)
 	var format = new ol.format.WKT();
-	feature = format.readFeature(wkt, {
+	var addFeature = format.readFeature(addWKT, {
 		dataProjection: 'EPSG:4326',
 		featureProjection: 'EPSG:28992'
 	});
-	pandenKaartlaagSource.addFeature(feature);
+	pandenKaartlaagSource.addFeature(addFeature);
 
 	var extent = pandenKaartlaagSource.getExtent();
-	map.getView().setCenter([extent[0],extent[1]]);
+	map.getView().setCenter([
+		(extent[0] + extent[2])/2,
+		(extent[1] + extent[3])/2
+	]);
 	map.getView().setZoom(11);
 }
 
-function getBAGpand(){
-		console.log('Er is geklikt. Maar de Cross-origin werkt nog niet lekker');
-		$.ajax({
-			url: "http://almere.pilod.nl/sparql?default-graph-uri=&query=SELECT+DISTINCT+%3Fgeom+%3Fhuisnummer+%3Fpc%0D%0AWHERE+%7B%0D%0A%3Fnummer+a+%3Chttp%3A%2F%2Fbag.kadaster.nl%2Fdef%23Nummeraanduiding%3E+.%0D%0A%3Fnummer+%3Chttp%3A%2F%2Fbag.kadaster.nl%2Fdef%23postcode%3E+%3Fpc+.%0D%0A%3Fnummer+%3Chttp%3A%2F%2Fbag.kadaster.nl%2Fdef%23huisnummer%3E+%3Fhuisnummer+.%0D%0A%3Fnummer+%3Chttp%3A%2F%2Fbag.kadaster.nl%2Fdef%23gerelateerdeOpenbareRuimte%3E+%3For+.%0D%0A%3Fvo+%3Chttp%3A%2F%2Fbag.kadaster.nl%2Fdef%23hoofdadres%3E+%3Fnummer+.+%0D%0A%3Fvo+%3Chttp%3A%2F%2Fbag.kadaster.nl%2Fdef%23onderdeelVan%3E+%3Fpand+.%0D%0A%3Fpand+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23hasGeometry%3E+%3Fgeompand+.%0D%0A%3Fgeompand+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23asWKT%3E+%3Fgeom+.%0D%0AFILTER+regex%28%3Fpc%2C+%221223LS%22%2C+%22i%22%29%0D%0AFILTER+%28xsd%3Ainteger%28%3Fhuisnummer%29+%3D+xsd%3Ainteger%289%29%29+.%0D%0A%7D+order+by+ASC%28%3Fpc%29+ASC%28%3Fhuisnummer%29&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on"
-		}).done(function(data) {
-			//TODO: loop over data, want het is niet zeker dat we maar 1 antwoord terug krijgen
-			removeAllFromPandenKaartlaag();
-			wkt=data.results.bindings[0].geom.value;
-			addWKTtoPandenKaartlaag();
-		});
+function getBAGpand(pc, hn){
+	//zoek het adres met pc en hn op via SPARQL en voeg alle gevonden records toe aan de kaart
+	var url = "http://almere.pilod.nl/sparql?default-graph-uri=&query=SELECT+DISTINCT+%3Fgeom+%3Fhuisnummer+%3Fpc%0D%0AWHERE+%7B%0D%0A%3Fnummer+a+%3Chttp%3A%2F%2Fbag.kadaster.nl%2Fdef%23Nummeraanduiding%3E+.%0D%0A%3Fnummer+%3Chttp%3A%2F%2Fbag.kadaster.nl%2Fdef%23postcode%3E+%3Fpc+.%0D%0A%3Fnummer+%3Chttp%3A%2F%2Fbag.kadaster.nl%2Fdef%23huisnummer%3E+%3Fhuisnummer+.%0D%0A%3Fnummer+%3Chttp%3A%2F%2Fbag.kadaster.nl%2Fdef%23gerelateerdeOpenbareRuimte%3E+%3For+.%0D%0A%3Fvo+%3Chttp%3A%2F%2Fbag.kadaster.nl%2Fdef%23hoofdadres%3E+%3Fnummer+.+%0D%0A%3Fvo+%3Chttp%3A%2F%2Fbag.kadaster.nl%2Fdef%23onderdeelVan%3E+%3Fpand+.%0D%0A%3Fpand+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23hasGeometry%3E+%3Fgeompand+.%0D%0A%3Fgeompand+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23asWKT%3E+%3Fgeom+.%0D%0AFILTER+regex%28%3Fpc%2C+%22" + pc + "%22%2C+%22i%22%29%0D%0AFILTER+%28xsd%3Ainteger%28%3Fhuisnummer%29+%3D+xsd%3Ainteger%28" + hn + "%29%29+.%0D%0A%7D+order+by+ASC%28%3Fpc%29+ASC%28%3Fhuisnummer%29&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on"
+	console.log('Er is geklikt. Maar de Cross-origin werkt nog niet lekker');
+	$.ajax({
+		url: url
+	}).done(function(data) {
+		removeAllFromPandenKaartlaag();
+		for (var binding in data.results.bindings){
+			var addWKT = data.results.bindings[binding].geom.value;
+			addWKTtoPandenKaartlaag(addWKT);
+		}
+	});
 }
